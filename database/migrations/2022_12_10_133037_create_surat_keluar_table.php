b178d340ce5159d27bdcbb7176ef436135ee6fed
Suryawandivta <gitdivta@gmail.com>
2022-12-10 21:32:22 +0800

    Make Table Migration

---
+++ database/migrations/2022_12_10_133037_create_surat_keluar_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surat_keluar', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_surat');
+            $table->date('tgl_keluar');
+            $table->string('perihal');
+            $table->string('sifat');
+            $table->string('lampiran');
+            $table->string('alamat');
+            $table->string('file_surat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surat_keluar');
+    }
+};
