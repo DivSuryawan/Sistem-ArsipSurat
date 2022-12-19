0ab4545c17dc0f5907e0fbbbf5ddcdb1910cb956
Suryawandivta <gitdivta@gmail.com>
2022-12-19 16:53:35 +0800

    modification sidebar

--- resources/views/Layouts/sidebar.blade.php
+++ resources/views/Layouts/sidebar.blade.php
@@ -26,60 +26,7 @@
                                 <span>Surat Masuk</span>
                             </a>
                         </li>
-                         <li class="sidebar-item  {{ request()->is('surat-keluar/index') ? 'active' :'' }}">
-                            <a href="/surat-keluar/index" class="sidebar-link">
-                              <i class="fa-solid fa-envelope-open"></i>
-                                <span>Surat Keluar</span>
-                            </a> 
-                        </li><hr>
-                        <li class="sidebar-item  ">
-                            <a href="#" class="sidebar-link" >
-                                <button type="button" class="btn btn-outline-primary block"
-                                            data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
-                                          <i class="fa-solid fa-right-from-bracket"></i>Log Out
-                                        </button>
-                            </a>
-                        </li>
-                       
+                        
             </div>
         </div>
 </div>
-{{-- modal log out --}}
-<div class="col-md-6 col-12">
-     <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-dialog-centered modal-dialog-scrollable"     role="document">
-                   <div class="modal-content">
-                        <div class="modal-header">
-                             <h5 class="modal-title" id="exampleModalCenterTitle">Log Out</h5>
-                               <button type="button" class="close" data-bs-dismiss="modal"aria-label="Close">
-                                  <i data-feather="x"></i>
-                                                        </button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        <p>
-                                                            Are You Sure ?
-                                                        </p>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-outline-danger"
-                                                            data-bs-dismiss="modal">
-                                                            <i class="bx bx-x d-block d-sm-none"></i>
-                                                            <span class="d-none d-sm-block">Close</span>
-                                                        </button>
-                                                       
-                                                        <button type="button" class="btn btn-outline-info">
-                                                             <a href="{{ route('logout') }}">
-                                                            <span class="d-none d-sm-block">Yes</span>
-                                                            </a>
-                                                        </button>
-                                                        
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
- 
-
