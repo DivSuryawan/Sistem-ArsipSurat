052718ac02c6fa0e82d3488adca1bb46b837808a
Suryawandivta <gitdivta@gmail.com>
2022-12-22 13:27:59 +0800

    modification view surat_masuk

--- resources/views/Layouts/script.blade.php
+++ resources/views/Layouts/script.blade.php
@@ -1,5 +1,5 @@
  <script src="{{ asset('dashboard/dist/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
-    <script src="{{ asset('dashboard/dist/assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script src="{{ asset('dashboard/dist/assets/js/pages/dashboard.js') }}"></script>
   <script src="{{ asset('dashboard/dist/assets/js/extensions/sweetalert2.js') }}"></script>
@@ -7,4 +7,10 @@
     <script src="{{ asset('dashboard/dist/assets/js/main.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+ <script src="{{ asset('dashboard/dist/assets/vendors/simple-datatables/simple-datatables.js') }}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script>
+        // Simple Datatable
+        let table1 = document.querySelector('#table1');
+        let dataTable = new simpleDatatables.DataTable(table1);
+    </script>
